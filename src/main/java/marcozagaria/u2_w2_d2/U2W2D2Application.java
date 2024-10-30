ac04e6cddd7dd56fdacd4470662eba09882faa7e
Marco Zagaria <marcozagaria@live.com>
2024-10-30 13:04:39 +0100

    Initial commit

---
+++ src/main/java/marcozagaria/u2_w2_d2/U2W2D2Application.java
@@ -0,0 +1,13 @@
+package marcozagaria.u2_w2_d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W2D2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W2D2Application.class, args);
+	}
+
+}
---
+++ src/test/java/marcozagaria/u2_w2_d2/U2W2D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package marcozagaria.u2_w2_d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W2D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
